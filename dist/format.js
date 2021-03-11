e46e8fb58639662f9e9174b267e7e8a6c50ea3ab
Philo Kaulkin <48894630+Phil0nator@users.noreply.github.com>
2021-03-10 18:30:46 -0800

    Format comments

--- dist/format.js
+++ dist/format.js
@@ -1,16 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Format = void 0;
+/**
+ * A Format object is used to classify a new type of message that will be sent over yodel.
+ * A Format consists of a series of {@linkcode Field} objects, which depict a single key-value pair,
+ * and a single type id called {@linkcode Format.mtype}.
+ * A Format object is used to construct a {@linkcode Section} object, which can be sent through yodel.
+ */
 var Format = /** @class */ (function () {
+    /**
+     * Construct a new {@linkcode Format} object.
+     * @param fields The {@linkcode Field} objects that define this new type
+     * @param mtype The identifier for this type (must sync between yodel participants)
+     */
     function Format(fields, mtype) {
         if (mtype === void 0) { mtype = 0; }
+        /**An identifier for this type*/
         this.mtype = 0;
         this.fields = fields;
         this.mtype = mtype;
     }
-    Format.prototype.stringify = function () {
-        return JSON.stringify({ "fields": this.fields.toString(), "mtype": this.mtype });
-    };
     return Format;
 }());
 exports.Format = Format;
--- src/format.ts
+++ src/format.ts
@@ -1,19 +1,26 @@
 import { Field, FieldType } from "./field"
 
-
+/**
+ * A Format object is used to classify a new type of message that will be sent over yodel.
+ * A Format consists of a series of {@linkcode Field} objects, which depict a single key-value pair, 
+ * and a single type id called {@linkcode Format.mtype}. 
+ * A Format object is used to construct a {@linkcode Section} object, which can be sent through yodel. 
+ */
 export class Format{
-
+    
+    /**The {@linkcode Field} objects that define this new type*/
     fields: Array<Field>;
+    /**An identifier for this type*/
     mtype: number = 0;
+
+    /**
+     * Construct a new {@linkcode Format} object.
+     * @param fields The {@linkcode Field} objects that define this new type
+     * @param mtype The identifier for this type (must sync between yodel participants)
+     */
     constructor(fields:Array<Field>, mtype:number=0){
         this.fields=fields;
         this.mtype = mtype;
-    }
-
-    stringify(){
-        return JSON.stringify(
-            {"fields":this.fields.toString(), "mtype":this.mtype}
-        );
     }
 
 
